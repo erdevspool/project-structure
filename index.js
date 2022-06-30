5d8c86716533b05dec5193cef9aeaa5ca3188e6f
Ahad hossain <ahadxx99@gmail.com>
2022-06-30 07:00:08 -0500

    product and Crud functionality

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const app = require('./app')
 
 
+app.get('/' , (req, res)=>{
+    res.send('Backend with Mongoose')
+  })
+
 app.listen(4000 , ()=>{
     console.log('My port is running at ' , 4000)
 })
